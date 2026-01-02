e136c852a766626acf9994dadc97ca4dba678909
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-01-02 10:13:16 +0530

    initial commit

---
+++ src/main/java/com/mycompany/feemanagementsystem/FeeManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.feemanagementsystem;
+
+/**
+ *
+ * @author Shubham Pathak
+ */
+public class FeeManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
